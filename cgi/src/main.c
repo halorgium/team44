afaabf51030f25598309534d088b94327b9b1a5d
tsm20 <tsm20>
2003-07-25 02:42:17 +0000

    starting

--- cgi/src/main.c
+++ cgi/src/main.c
@@ -1,3 +1,26 @@
-/******************************/
-/*
+#include <stdio.h>
+#include "cgic.h"
+#include "../../structs.h"
+
+int cgiMain() {
+#if DEBUG
+    /* Load a saved CGI scenario if we're debugging */
+    cgiReadEnvironment("/home/boutell/public_html/capcgi.dat");
+#endif
+    cgiHeaderContentType("text/html");
+    fprintf(cgiOut, "<HTML><HEAD>\n");
+    fprintf(cgiOut, "<TITLE>cgic test</TITLE></HEAD>\n");
+    fprintf(cgiOut, "<BODY><H1>cgic test</H1>\n");
+    Name();
+    Address();
+    Hungry();
+    Temperature();
+    Frogs();
+    Color();
+    Flavors();
+    NonExButtons();
+    RadioButtons();
+    fprintf(cgiOut, "</BODY></HTML>\n");
+    return 0;
+}
 
