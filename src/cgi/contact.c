1e9c4a259d56073f6f8b7da3f4c57fb83bb76edc
jbk20 <jbk20>
2003-09-15 02:26:57 +0000

    getting dan bak

--- src/cgi/contact.c
+++ src/cgi/contact.c
@@ -76,7 +76,7 @@ void printContact(void) {
   fprintf(cgiOut, "  </tr>\n");
   fprintf(cgiOut, "  <tr>\n");
   fprintf(cgiOut, "    <td class=\"describe\">\n");
-  fprintf(cgiOut, "      <label title=\"Position\">Way better than dan!</label>\n");
+  fprintf(cgiOut, "      <label title=\"Position\">Manages the Kwike Mart</label>\n");
   fprintf(cgiOut, "    </td>\n");
   fprintf(cgiOut, "    <td>");
   fprintf(cgiOut, "    </td>\n");
