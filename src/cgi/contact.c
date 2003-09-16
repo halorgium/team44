09d04b8522d538e18937b7436afa0d8d3dea030c
jbk20 <jbk20>
2003-09-16 21:11:50 +0000

    dan smells

--- src/cgi/contact.c
+++ src/cgi/contact.c
@@ -76,7 +76,7 @@ void printContact(void) {
   fprintf(cgiOut, "  </tr>\n");
   fprintf(cgiOut, "  <tr>\n");
   fprintf(cgiOut, "    <td class=\"describe\">\n");
-  fprintf(cgiOut, "      <label title=\"Position\">Has giant jaffa testicles</label>\n");
+  fprintf(cgiOut, "      <label title=\"Position\">Manages daniel the baby</label>\n");
   fprintf(cgiOut, "    </td>\n");
   fprintf(cgiOut, "    <td>");
   fprintf(cgiOut, "    </td>\n");
